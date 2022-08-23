c1c2f347b20e902dfbebff888fbf8c21d449cde5
Jared McMahan <jaredm788@gmail.com>
2022-08-22 21:54:28 -0600

    commit tree d19330f6b3491c33a3f0a0faf4cc5ac1a35ae56f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let arrayNames = []
+
+
+function writeCards(array,eventName) {
+    for(let i = 0; i < array.length; i++ ) {
+        let message = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`
+        arrayNames.push(message)
+        
+    }
+    return arrayNames
+}
+
+function countDown() {
+    let countDown = 0
+    while (countDown < 11) {
+        console.log(countDown++)
+    }
+}
